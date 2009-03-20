29d2d460f307d1a966541b80f688cfe55bca0d2a
asivache <asivache@348d0f76-0448-11de-a6fe-93d51630548a>
2009-03-20 05:08:15 +0000

    a trivial interface and even more trivial implementations that do nothing (ignore the data they receive)
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@107 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ playground/java/src/org/broadinstitute/sting/indels/DiscardingPileReceiver.java
@@ -0,0 +1,19 @@
+package org.broadinstitute.sting.indels;
+
+import net.sf.samtools.SAMRecord;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: asivache
+ * Date: Mar 20, 2009
+ * Time: 12:55:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DiscardingPileReceiver implements RecordPileReceiver {
+    @Override
+    public void receive(Collection<SAMRecord> c) {
+        return ; // do nothing, discard the pile.
+    }
+}
---
+++ playground/java/src/org/broadinstitute/sting/indels/RecordPileReceiver.java
@@ -0,0 +1,22 @@
+package org.broadinstitute.sting.indels;
+
+import net.sf.samtools.SAMRecord;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: asivache
+ * Date: Mar 19, 2009
+ * Time: 7:51:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/** This interface abstracts processing of piles (collections) of SAM records.
+ * Its only receive() method should be called to send a collection of records
+ * to the implementation.
+ */
+
+public interface RecordPileReceiver {
+    public void receive(Collection<SAMRecord> c) ;
+}
