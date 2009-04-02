d202264b2307ecf87781a479ff0ad8329eef4883
jmaguire <jmaguire@348d0f76-0448-11de-a6fe-93d51630548a>
2009-04-02 17:55:40 +0000

    initial add of pooled calling experiment walker.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@262 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ java/src/org/broadinstitute/sting/playground/gatk/walkers/PoolCallingExperiment.java
@@ -0,0 +1,50 @@
+
+package org.broadinstitute.sting.playground.gatk.walkers;
+
+import org.broadinstitute.sting.gatk.refdata.ReferenceOrderedDatum;
+import org.broadinstitute.sting.gatk.refdata.rodDbSNP;
+import org.broadinstitute.sting.gatk.refdata.rodGFF;
+import org.broadinstitute.sting.gatk.walkers.LocusWalker;
+import org.broadinstitute.sting.gatk.LocusContext;
+import org.broadinstitute.sting.playground.gatk.walkers.AlleleFrequencyWalker;
+import org.broadinstitute.sting.playground.utils.AlleleFrequencyEstimate;
+import org.broadinstitute.sting.utils.cmdLine.Argument;
+
+import java.util.List;
+
+public class PoolCallingExperiment extends LocusWalker<AlleleFrequencyEstimate, String> 
+{
+    List<AlleleFrequencyWalker> deep_callers;
+    List<AlleleFrequencyWalker> shallow_callers;
+    AlleleFrequencyWalker       pooled_caller;
+
+    @Argument public int DOWNSAMPLE;
+
+    public AlleleFrequencyEstimate map(List<ReferenceOrderedDatum> rodData, char ref, LocusContext context) 
+    {
+        for (int i = 0; i < context.getReads().size(); i++)
+        {
+            String read_group = (String)(context.getReads().get(i).getAttribute("RG"));
+            String sample     = context.getReads().get(i).getHeader().getReadGroup(read_group).READ_GROUP_SAMPLE_TAG;
+            System.out.println("RG: " + read_group + " SAMPLE: " + sample);
+        } 
+        return null;
+    }
+
+    public void onTraversalDone() 
+    {
+        return;
+    }
+
+    public String reduceInit() 
+    { 
+        return "";
+    }
+
+    public String reduce(AlleleFrequencyEstimate alleleFreq, String sum) 
+    {
+        return "";
+    }
+
+    
+}
