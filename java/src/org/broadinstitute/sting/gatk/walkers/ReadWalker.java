f2f9fa3ed4babbfa942cd0b18420b25b1b9d0d10
asivache <asivache@348d0f76-0448-11de-a6fe-93d51630548a>
2009-04-17 16:43:25 +0000

    doc added
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@464 348d0f76-0448-11de-a6fe-93d51630548a

--- java/src/org/broadinstitute/sting/gatk/walkers/ReadWalker.java
+++ java/src/org/broadinstitute/sting/gatk/walkers/ReadWalker.java
@@ -14,6 +14,9 @@ public abstract class ReadWalker<MapType, ReduceType> extends Walker<MapType, Re
     public boolean requiresOrderedReads() { return false; }
     
     // Do we actually want to operate on the context?
+    /** Must return true for reads that need to be processed. Reads, for which this method return false will
+     * be skipped by the engine and never passed to the walker.
+     */
     public boolean filter(LocusContext context, SAMRecord read) {
         // We are keeping all the reads
         return true;
