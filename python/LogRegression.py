38c2f734572054c61efc45ec7e4b921d99ae312f
andrewk <andrewk@348d0f76-0448-11de-a6fe-93d51630548a>
2009-04-24 18:31:26 +0000

    LogRegression.py script that converts parameter files for each dinucleotide regression into one file to be read in by correction script.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@528 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ python/LogRegression.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+
+dinuc_root = sys.argv[1]
+fout = file(dinuc_root+".log_reg_params", "w")
+
+fout.write("p,q\t")
+for p in range(5):
+    for q in range(5):
+        fout.write("%d,%d\t" % (p,q))
+fout.write("\n")
+
+for dinuc in ("AA","AC","AG","AT","CA","CC","CG","CT","GA","GC","GG","GT","TA","TC","TG","TT"):
+    dinin = open(dinuc_root+"."+dinuc+".parameters")
+    dinin.readline()
+    params = []
+    for line in dinin:
+        line.rstrip("\n")
+        params.extend(map(float, line.split()))
+    fout.write(dinuc+"\t")
+    fout.write("\t".join(map(str, params)))
+    fout.write("\n")
+        
+
+
+
+
+
+
+
