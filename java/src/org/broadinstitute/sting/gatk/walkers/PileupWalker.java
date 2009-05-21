65995887fc2614c64c0fbc01363eedda9729d5e2
depristo <depristo@348d0f76-0448-11de-a6fe-93d51630548a>
2009-05-21 22:25:37 +0000

    Releasable version of the Pileup walker
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@786 348d0f76-0448-11de-a6fe-93d51630548a

--- java/src/org/broadinstitute/sting/gatk/walkers/PileupWalker.java
+++ java/src/org/broadinstitute/sting/gatk/walkers/PileupWalker.java
@@ -6,29 +6,42 @@ import org.broadinstitute.sting.gatk.refdata.rodDbSNP;
 import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
 import org.broadinstitute.sting.utils.cmdLine.Argument;
 import org.broadinstitute.sting.utils.ReadBackedPileup;
+import org.broadinstitute.sting.utils.Utils;
+import net.sf.samtools.SAMRecord;
+
+import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
- * User: mdepristo
- * Date: Feb 22, 2009
- * Time: 3:22:14 PM
- * To change this template use File | Settings | File Templates.
+ * samtools pileup [-f in.ref.fasta] [-t in.ref_list] [-l in.site_list] [-iscg] [-T theta] [-N nHap] [-r pairDiffRate] <in.alignment>
+ *
+ * Print the alignment in the pileup format. In the pileup format, each line represents a genomic position,
+ * consisting of chromosome name, coordinate, reference base, read bases, read qualities and alignment mapping
+ * qualities. Information on match, mismatch, indel, strand, mapping quality and start and end of a read are all
+ * encoded at the read base column. At this column, a dot stands for a match to the reference base on the forward strand,
+ * a comma for a match on the reverse strand, ‘ACGTN’ for a mismatch on the forward strand and ‘acgtn’ for a mismatch on the
+ * reverse strand.
+ *
+ * A pattern ‘\+[0-9]+[ACGTNacgtn]+’ indicates there is an insertion between this reference position and the next
+ * reference position. The length of the insertion is given by the integer in the pattern, followed by the inserted sequence.
+ * Similarly, a pattern ‘-[0-9]+[ACGTNacgtn]+’ represents a deletion from the reference.
+ * Also at the read base column, a symbol ‘^’ marks the start of a read segment which is a contiguous subsequence on the read
+ * separated by ‘N/S/H’ CIGAR operations. The ASCII of the character following ‘^’ minus 33 gives the mapping quality.
+ * A symbol ‘$’ marks the end of a read segment.
  */
 public class PileupWalker extends LocusWalker<Integer, Integer> implements TreeReducible<Integer> {
-    @Argument(fullName="verbose",doc="verbose",required=false)
-    public boolean VERBOSE = false;
+    @Argument(fullName="alwaysShowSecondBase",doc="If true, prints dummy bases for the second bases in the BAM file where they are missing",required=false)
+    public boolean alwaysShowSecondBase = false;
+
+    @Argument(fullName="showSecondBaseQuals",doc="If true, prints out second base qualities in the pileup",required=false)
+    public boolean showSecondBaseQuals = false;
+
 
     @Argument(fullName="extended",shortName="ext",doc="extended",required=false)
     public boolean EXTENDED = false;
-    
+
     public boolean FLAG_UNCOVERED_BASES = true;     // todo: how do I make this a command line argument?
     
     public void initialize() {
-    }
-
-    // Do we actually want to operate on the context?
-    public boolean filter(RefMetaDataTracker tracker, char ref, LocusContext context) {
-        return true;    // We are keeping all the reads
     }
 
     public Integer map(RefMetaDataTracker tracker, char ref, LocusContext context) {
@@ -36,13 +49,22 @@ public class PileupWalker extends LocusWalker<Integer, Integer> implements TreeR
         String bases = pileup.getBases();
         
         if ( bases.equals("") && FLAG_UNCOVERED_BASES ) {
-            bases = "*** UNCOVERED SITE ***";
+            bases = "***UNCOVERED_SITE***";
         }
 
-        String extras = "";
-        if ( VERBOSE ) {
-            extras += " BQ=" + pileup.getQualsAsInts();
-            extras += " MQ=" + pileup.getMappingQualsAsInts();
+        StringBuilder extras = new StringBuilder();
+
+        String secondBasePileup = pileup.getSecondaryBasePileup();
+        if ( secondBasePileup == null && alwaysShowSecondBase ) {
+            secondBasePileup = Utils.dupString('N', bases.length());
+        }
+        if ( secondBasePileup != null ) extras.append(" ").append(secondBasePileup);
+
+        if ( showSecondBaseQuals ) {
+            String secondQualPileup = pileup.getSecondaryQualPileup();
+            if ( secondQualPileup == null )
+             secondQualPileup = Utils.dupString((char)(33), bases.length());
+            extras.append(" ").append(secondQualPileup);
         }
 
         String rodString = "";
