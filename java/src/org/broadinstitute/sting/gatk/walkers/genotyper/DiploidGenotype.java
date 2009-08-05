34af669dbb8fd7cb1c99b08649e8c41eb019960b
depristo <depristo@348d0f76-0448-11de-a6fe-93d51630548a>
2009-08-05 00:53:43 +0000

    Explicit ENUM representation of the diploid genotypes.  Please use this from now on to represent strings like AA or AT
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@1380 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ java/src/org/broadinstitute/sting/gatk/walkers/genotyper/DiploidGenotype.java
@@ -0,0 +1,21 @@
+package org.broadinstitute.sting.gatk.walkers.genotyper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: depristo
+ * Date: Aug 4, 2009
+ * Time: 6:46:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum DiploidGenotype {
+    AA,
+    AC,
+    AG,
+    AT,
+    CC,
+    CG,
+    CT,
+    GG,
+    GT,
+    TT
+}
