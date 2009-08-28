ab9458d06d2bfd32baef3c498bab55f74900f4d1
depristo <depristo@348d0f76-0448-11de-a6fe-93d51630548a>
2009-08-28 00:15:01 +0000

    support for scala walkers
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@1468 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ scala/src/PrintReadsScala.scala
@@ -0,0 +1,25 @@
+package org.broadinstitute.sting.scala
+
+import org.broadinstitute.sting.gatk.walkers.LocusWalker
+import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker
+import org.broadinstitute.sting.gatk.contexts.ReferenceContext
+import org.broadinstitute.sting.gatk.contexts.AlignmentContext
+
+class PrintReadsScala extends LocusWalker[Int,Int] {
+    override def map(tracker: RefMetaDataTracker, ref: ReferenceContext, context: AlignmentContext): Int = {
+        //println(context.getReads().size())
+        return 1
+    }
+
+    def reduceInit(): Int = {
+        return 0;
+    }
+
+    def reduce(m: Int, r: Int): Int = {
+        return m + r;
+    }
+    
+    def main(args: Array[String]) {
+       println("Hello, world!")
+    }    
+}
