6c7a30066492003c5802e238bca2758e6997546c
depristo <depristo@348d0f76-0448-11de-a6fe-93d51630548a>
2009-09-12 19:17:09 +0000

    Missing file
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@1601 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ java/src/org/broadinstitute/sting/gatk/datasources/providers/ReferenceOrderedView.java
@@ -0,0 +1,8 @@
+package org.broadinstitute.sting.gatk.datasources.providers;
+
+import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
+import org.broadinstitute.sting.utils.GenomeLoc;
+
+public interface ReferenceOrderedView extends View {
+    RefMetaDataTracker getReferenceOrderedDataAtLocus( GenomeLoc loc );
+}
