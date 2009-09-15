6783fda42a8394bfb304d43feb8c11d556ce1c30
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2009-09-15 01:56:08 +0000

    Updated unit test to reflect changes to vcf output
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@1623 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/filters/VariantFiltrationIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/filters/VariantFiltrationIntegrationTest.java
@@ -9,49 +9,49 @@ public class VariantFiltrationIntegrationTest extends WalkerTest {
     @Test
     public void testIntervals() {
 
-        String[] md5DoC = {"492599c4d7d6dfca29659a7be3e3b7d4", "21c8e1f9dc65fdfb39347547f9b04011"};
+        String[] md5DoC = {"5f176d27eba04c8e08b54b60fd96758e", "21c8e1f9dc65fdfb39347547f9b04011"};
         WalkerTestSpec spec1 = new WalkerTestSpec(
                 "-T VariantFiltration -X DepthOfCoverage:max=70 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
                  Arrays.asList(md5DoC));
         executeTest("testDoCFilter", spec1);
 
-        String[] md5AlleleBalance = {"33ce9e974efa7c095e3124f0ecad14b3", "a13e4ce6260bf9f33ca99dc808b8e6ad"};
+        String[] md5AlleleBalance = {"dfa47f9465214445b737ec49025d6b5f", "a13e4ce6260bf9f33ca99dc808b8e6ad"};
         WalkerTestSpec spec2 = new WalkerTestSpec(
                 "-T VariantFiltration -X AlleleBalance:low=0.25,high=0.75 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
                  Arrays.asList(md5AlleleBalance));
         executeTest("testAlleleBalanceFilter", spec2);
 
-        String[] md5Strand = {"f30ca865290e1c3537a8a30e4c3a5df2", "0f7db0aad764268ee8fa3b857df8d87d"};
+        String[] md5Strand = {"2d32ddd6daccd08f3df148f4d373fa5e", "0f7db0aad764268ee8fa3b857df8d87d"};
         WalkerTestSpec spec3 = new WalkerTestSpec(
                 "-T VariantFiltration -X FisherStrand:pvalue=0.0001 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
                  Arrays.asList(md5Strand));
         executeTest("testStrandFilter", spec3);
 
-        String[] md5Lod = {"948963861d9d2260e9f5ed6447aa30cb", "7e0c4f2b0fda85fd2891eee76c396a55"};
+        String[] md5Lod = {"157e7beab140f4252ed41ebfc9a508f2", "7e0c4f2b0fda85fd2891eee76c396a55"};
         WalkerTestSpec spec4 = new WalkerTestSpec(
                 "-T VariantFiltration -X LodThreshold:lod=10 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
                  Arrays.asList(md5Lod));
         executeTest("testLodFilter", spec4);
 
-        String[] md5MQ0 = {"57ac3b2df0590fe189e4462560cba686", "3203de335621851bccf596242b079e23"};
+        String[] md5MQ0 = {"15738f3717dfa3192566a7ddf301fe11", "3203de335621851bccf596242b079e23"};
         WalkerTestSpec spec5 = new WalkerTestSpec(
                 "-T VariantFiltration -X MappingQualityZero:max=70 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
                  Arrays.asList(md5MQ0));
         executeTest("testMappingQuality0Filter", spec5);
 
-        String[] md5MQ = {"b8306b693dfee52113d8ecb405ddf25a", "ecc777feedea61f7b570d114c2ab89b1"};
+        String[] md5MQ = {"e7fb1f166d2359f653f98f8215215aca", "ecc777feedea61f7b570d114c2ab89b1"};
         WalkerTestSpec spec6 = new WalkerTestSpec(
                 "-T VariantFiltration -X MappingQuality:min=20 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
                  Arrays.asList(md5MQ));
         executeTest("testRMSMappingQualityFilter", spec6);
 
-        String[] md5OnOff = {"8f35d20e9b53ed5c5a161b15501705bf", "67f2e1bc025833b0fa31f47195198997"};
+        String[] md5OnOff = {"fe4acc1505f19480f275d7a96b0e3ddb", "67f2e1bc025833b0fa31f47195198997"};
         WalkerTestSpec spec7 = new WalkerTestSpec(
                 "-T VariantFiltration -X OnOffGenotypeRatio:threshold=0.9 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.1kg.p2.chr1_10mb_11_mb.SLX.bam -L 1:10,000,000-11,000,000 -B variant,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.geli.calls -D /humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -vcf %s -included %s -sample NA12878",
                  2,
