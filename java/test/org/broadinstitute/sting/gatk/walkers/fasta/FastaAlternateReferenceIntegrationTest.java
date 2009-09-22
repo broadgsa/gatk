6780476fb5ab15d3b821dbd0cbee9852a049a363
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2009-09-22 19:46:32 +0000

    updated to deal with new dbSNP rod
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@1687 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/fasta/FastaAlternateReferenceIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/fasta/FastaAlternateReferenceIntegrationTest.java
@@ -18,13 +18,13 @@ public class FastaAlternateReferenceIntegrationTest extends WalkerTest {
         WalkerTestSpec spec2 = new WalkerTestSpec(
                 "-T FastaAlternateReferenceMaker -R /broad/1KG/reference/human_b36_both.fasta -B indels,PointIndel,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.indels -B snpmask,dbsnp,/humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -L 1:10,075,000-10,075,380;1:10,093,447-10,093,847;1:10,271,252-10,271,452 -o %s",
                  1,
-                 Arrays.asList("b7c0d95c1bf1ea69f5a7a58dd4d57685"));
+                 Arrays.asList("3a48986c3832a768b478c3e95f994b0f"));
         executeTest("testFastaAlternateReferenceIndels", spec2);
 
         WalkerTestSpec spec3 = new WalkerTestSpec(
                 "-T FastaAlternateReferenceMaker -sequenom -R /broad/1KG/reference/human_b36_both.fasta -B indels,PointIndel,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chr1_10mb_11mb.slx.indels -B snpmask,dbsnp,/humgen/gsa-scr1/GATK_Data/dbsnp_129_b36.rod -L 1:10,075,000-10,075,380;1:10,093,447-10,093,847;1:10,271,252-10,271,452 -o %s",
                  1,
-                 Arrays.asList("3137b0c90ba13fb648be51c14fdb296f"));
+                 Arrays.asList("b53b067a3bdada202dba7d3c382ae1d0"));
         executeTest("testFastaAlternateReferenceIndelSequenom", spec3);
 	
         WalkerTestSpec spec4 = new WalkerTestSpec(
