c9c8fd1fef4b89d0c214a469b15e3f2fab420fad
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2009-10-14 01:24:06 +0000

    Added the discovery LOD score to the meta data
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@1825 348d0f76-0448-11de-a6fe-93d51630548a

--- java/src/org/broadinstitute/sting/utils/genotype/GenotypeMetaData.java
+++ java/src/org/broadinstitute/sting/utils/genotype/GenotypeMetaData.java
@@ -10,6 +10,9 @@ package org.broadinstitute.sting.utils.genotype;
  */
 public class GenotypeMetaData {
 
+    // the discovery lod score
+    private double mLOD;
+
     // the strand score lod
     private double mSLOD;
 
@@ -19,12 +22,23 @@ public class GenotypeMetaData {
     /**
      * create a basic genotype meta data pbject, given the following fields
      *
+     * @param discoveryLOD       the discovery lod
      * @param strandLOD          the strand score lod
      * @param alleleFrequency    the allele frequency
      */
-    public GenotypeMetaData(double strandLOD, double alleleFrequency) {
+    public GenotypeMetaData(double discoveryLOD, double strandLOD, double alleleFrequency) {
+        mLOD = discoveryLOD;
         mSLOD = strandLOD;
         mAlleleFrequency = alleleFrequency;
+    }
+
+    /**
+     * get the discovery lod
+     *
+     * @return the discovery lod
+     */
+    public double getLOD() {
+        return mLOD;
     }
 
     /**
