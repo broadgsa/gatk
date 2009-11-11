637f02f392bc0c71d41f108090153b63d11d52da
hanna <hanna@348d0f76-0448-11de-a6fe-93d51630548a>
2009-11-11 16:48:21 +0000

    BWA/C changes have arrived in svn repository.  Remove temporary storage from Sting repository.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@2019 348d0f76-0448-11de-a6fe-93d51630548a

--- c/bwa/build_mac.sh
+++ c/bwa/build_mac.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export BWA_HOME="/Users/mhanna/src/bwa-0.5.3"
+export BWA_HOME="/Users/mhanna/src/bwa"
 export JAVA_INCLUDE="/System/Library/Frameworks/JavaVM.framework/Headers"
 export TARGET_LIB="libbwa.dylib"
 export EXTRA_LIBS="-lc -lz -lsupc++"
