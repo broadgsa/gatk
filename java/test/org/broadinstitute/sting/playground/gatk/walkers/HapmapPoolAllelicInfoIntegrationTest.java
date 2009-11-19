306f4624c66ce04302c940fd4cc36f2fc439423a
chartl <chartl@348d0f76-0448-11de-a6fe-93d51630548a>
2009-11-19 18:22:29 +0000

    oops forgot to update the md5s
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@2093 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/playground/gatk/walkers/HapmapPoolAllelicInfoIntegrationTest.java
+++ java/test/org/broadinstitute/sting/playground/gatk/walkers/HapmapPoolAllelicInfoIntegrationTest.java
@@ -22,7 +22,7 @@ public class HapmapPoolAllelicInfoIntegrationTest extends WalkerTest {
                 + "-I /humgen/gsa-scr1/GATK_Data/Validation_Data/FHSP_pool3_test.bam "
                 + "-R /seq/references/Homo_sapiens_assembly18/v0/Homo_sapiens_assembly18.fasta -of %s "
                 + "-ps 40 -L /humgen/gsa-scr1/GATK_Data/Validation_Data/FHS_test_intervals.interval_list";
-        String md5ForThisTest = "9e44a04ea574a7f76efbc3a1f8cc710a";
+        String md5ForThisTest = "da1222d7514f247eae066134d8e3cde3";
         WalkerTestSpec spec = new WalkerTestSpec(test_args, 1, Arrays.asList(md5ForThisTest));
         executeTest("Pool 3 of FHS Pilot on testbed intervals", spec);
     }
@@ -35,7 +35,7 @@ public class HapmapPoolAllelicInfoIntegrationTest extends WalkerTest {
                 + "-I /humgen/gsa-scr1/GATK_Data/Validation_Data/FHSP_pool3_test.bam "
                 + "-R /seq/references/Homo_sapiens_assembly18/v0/Homo_sapiens_assembly18.fasta -of %s "
                 + "-ps 40";
-        String md5ForThisTest = "571c10a635a84d0bbd453897ef4fb3c6";
+        String md5ForThisTest = "120f3307d94d613c3559a1051fe3aaef";
         WalkerTestSpec spec = new WalkerTestSpec(test_args, 1, Arrays.asList(md5ForThisTest));
         executeTest("Pool 3 of FHS Pilot without testbed intervals", spec);
     }
@@ -48,7 +48,7 @@ public class HapmapPoolAllelicInfoIntegrationTest extends WalkerTest {
                 +"-I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12891.a2b.recal.annotation_subset.bam "
                 +"-L chr1:14000000-18000000 -B calls,Variants,/humgen/gsa-scr1/GATK_Data/Validation_Data/NA12891.a2b.ssg1b.geli.calls "
                 +"-of %s -R /seq/references/Homo_sapiens_assembly18/v0/Homo_sapiens_assembly18.fasta -ps 2";
-        String md5ForThisTest = "46c6a22ab57c40453f29b99a57288e0e";
+        String md5ForThisTest = "cc81a9d8279e7b6ad50f9dc1f19a7419";
         WalkerTestSpec spec = new WalkerTestSpec(test_args, 1, Arrays.asList(md5ForThisTest));
         executeTest("Test on pool of two individuals -- CEU father+daughter chips against subset of the father's calls; power at lod 3", spec);
 
