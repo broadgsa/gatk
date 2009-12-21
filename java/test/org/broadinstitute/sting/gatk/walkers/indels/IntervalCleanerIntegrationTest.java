87e5a41964541fe5bda7e6974ee77bb8ba5e60c6
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2009-12-21 05:46:16 +0000

    Fixed a bug that accounted for a bunch of my remaining mis-cleaned indels. Also, slightly optimized the cleaner by using readBases (instead of readString) and caching cigar element lengths.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@2419 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/indels/IntervalCleanerIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/indels/IntervalCleanerIntegrationTest.java
@@ -9,21 +9,21 @@ public class IntervalCleanerIntegrationTest extends WalkerTest {
     @Test
     public void testIntervals() {
 
-        String[] md5lod5 = {"163d2f1b04741986151636d3c7e04c94", "4aa3637e86822c95af3e2c9b414530c3"};
+        String[] md5lod5 = {"042c1c2649a51a260bc204ec5f256c1a", "460631e8d98644dcf53b3045ca40f02a"};
         WalkerTestSpec spec1 = new WalkerTestSpec(
                 "-T IntervalCleaner -LOD 5 -maxConsensuses 100 -greedy 100 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chrom1.SLX.SRP000032.2009_06.bam -L /humgen/gsa-scr1/GATK_Data/Validation_Data/cleaner.test.intervals --OutputCleaned %s -snps %s",
                  2,
                  Arrays.asList(md5lod5));
         executeTest("testLod5", spec1);
 
-        String[] md5lod200 = {"1d89ee2af03df79eb5de494c77767221", "e39aa718c9810364ebe30964d878d5ff"};
+        String[] md5lod200 = {"1d89ee2af03df79eb5de494c77767221", "6137bf0c25c7972b07b0d3fc6979cf5b"};
         WalkerTestSpec spec2 = new WalkerTestSpec(
                 "-T IntervalCleaner -LOD 200 -maxConsensuses 100 -greedy 100 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chrom1.SLX.SRP000032.2009_06.bam -L /humgen/gsa-scr1/GATK_Data/Validation_Data/cleaner.test.intervals --OutputCleaned %s -snps %s",
                  2,
                  Arrays.asList(md5lod200));
         executeTest("testLod200", spec2);
 
-        String[] md5cleanedOnly = {"710f9114ec496c73f1c3782b5cb09757", "4aa3637e86822c95af3e2c9b414530c3"};
+        String[] md5cleanedOnly = {"168c8d02fceb107477381b93d189fe1f", "460631e8d98644dcf53b3045ca40f02a"};
         WalkerTestSpec spec3 = new WalkerTestSpec(
                 "-T IntervalCleaner -LOD 5 -cleanedOnly -maxConsensuses 100 -greedy 100 -R /broad/1KG/reference/human_b36_both.fasta -I /humgen/gsa-scr1/GATK_Data/Validation_Data/NA12878.chrom1.SLX.SRP000032.2009_06.bam -L /humgen/gsa-scr1/GATK_Data/Validation_Data/cleaner.test.intervals --OutputCleaned %s -snps %s",
                  2,
