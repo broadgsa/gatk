e491b42951af93429458cc7b5c5d3e8a61a2e3f1
chartl <chartl@348d0f76-0448-11de-a6fe-93d51630548a>
2010-02-11 14:09:30 +0000

    Dumb little script that grabs Picard metrics (alignment, hybrid selection, insert size) from picard_aggregation given the path to the bam file; zips them up, and spits them out; for use with Firehose
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@2824 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ python/makeMetricsFilesForFirehose.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+bam_file = sys.argv[1]
+project = sys.argv[2]
+directory = bam_file.rsplit("/",1)[0]+"/"
+sample_id = bam_file.rsplit("/",1)[1].rsplit(".",1)[0]
+is_metrics = directory+sample_id+".insert_size_metrics"
+his_metrics = directory+sample_id+".hybrid_selection_metrics"
+ali_metrics = directory+sample_id+".alignment_summary_metrics"
+os.system("zip -j "+project+"_"+sample_id+"_sequencing_metrics"+" "+is_metrics+" "+his_metrics+" "+ali_metrics)
