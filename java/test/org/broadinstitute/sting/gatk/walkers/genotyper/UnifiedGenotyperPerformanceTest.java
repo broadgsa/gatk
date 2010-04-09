fa018762555d53ee270b4e1d4edea8e613953aa0
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2010-04-09 19:42:29 +0000

    UnifiedGenotyper performance tests (WG, WEx); currently takes just over an hour.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@3148 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ java/test/org/broadinstitute/sting/gatk/walkers/genotyper/UnifiedGenotyperPerformanceTest.java
@@ -0,0 +1,35 @@
+package org.broadinstitute.sting.gatk.walkers.genotyper;
+
+import org.broadinstitute.sting.WalkerTest;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class UnifiedGenotyperPerformanceTest extends WalkerTest {
+
+    @Test
+    public void testUnifiedGenotyperWholeGenome() {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                "-R " + seqLocation + "references/Homo_sapiens_assembly18/v0/Homo_sapiens_assembly18.fasta" +
+                        " -T UnifiedGenotyper" +
+                        " -I " + evaluationDataLocation + "NA12878.GAII.chr1.50MB.bam" +
+                        " -L chr1:1-50,000,000" +
+                        " -varout /dev/null",
+                0,
+                new ArrayList(0));
+        executeTest("testUnifiedGenotyperWholeGenome", spec);
+    }
+
+    @Test
+    public void testUnifiedGenotyperWholeExome() {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                "-R " + seqLocation + "references/Homo_sapiens_assembly18/v0/Homo_sapiens_assembly18.fasta" +
+                        " -T UnifiedGenotyper" +
+                        " -I " + evaluationDataLocation + "NA12878.ESP.WEx.chr1.bam" +
+                        " -L " + evaluationDataLocation + "whole_exome_agilent_designed_120.targets.chr1.interval_list" +
+                        " -varout /dev/null",
+                0,
+                new ArrayList(0));
+        executeTest("testUnifiedGenotyperWholeExome", spec);
+    }
+}
