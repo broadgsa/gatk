cdc175f7e3a05a272b62af251c542919700d8965
asivache <asivache@348d0f76-0448-11de-a6fe-93d51630548a>
2010-04-20 17:41:52 +0000

    Synchronizing version to make sure everything compiles; this model is not operational yet
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@3214 348d0f76-0448-11de-a6fe-93d51630548a

--- java/src/org/broadinstitute/sting/gatk/walkers/genotyper/SimpleIndelCalculationModel.java
+++ java/src/org/broadinstitute/sting/gatk/walkers/genotyper/SimpleIndelCalculationModel.java
@@ -16,15 +16,29 @@ public class SimpleIndelCalculationModel extends GenotypeCalculationModel {
 
     protected SimpleIndelCalculationModel() {}
 
+    private int testSkipCount = 5;
+
     public VariantCallContext callLocus(RefMetaDataTracker tracker, char ref, GenomeLoc loc, Map<String, StratifiedAlignmentContext> contexts, DiploidGenotypePriors priors) {
         cachedContext = contexts;
         return null;
     }
 
-    public VariantCallContext callExtendedLocus(RefMetaDataTracker tracker, char ref, GenomeLoc loc, Map<String, StratifiedAlignmentContext> contexts) {
+    public VariantCallContext callExtendedLocus(RefMetaDataTracker tracker, char[] ref, GenomeLoc loc, Map<String, StratifiedAlignmentContext> contexts) {
 
-        System.out.println("Reached " + loc + " through an extended event");
-
+        System.out.println("\nReached " + loc + " through an extended event");
+        for (Map.Entry<String,StratifiedAlignmentContext> e : contexts.entrySet()) {
+            System.out.println("Set "+e.getKey());
+            System.out.println("  Context: "+e.getValue().getContext(StratifiedAlignmentContext.StratifiedContextType.COMPLETE).size()+ " reads");
+            System.out.println("  Cached context: "+
+                      cachedContext.get(e.getKey()).getContext(StratifiedAlignmentContext.StratifiedContextType.COMPLETE).size()+ " reads");
+            System.out.println("  First read in cached context: "+
+                      cachedContext.get(e.getKey()).getContext(StratifiedAlignmentContext.StratifiedContextType.COMPLETE).getBasePileup().getReads().get(0).getReadName());
+            ReadBackedExtendedEventPileup p = e.getValue().getContext(StratifiedAlignmentContext.StratifiedContextType.COMPLETE).getExtendedEventPileup();
+            if ( p== null ) System.out.println("EXTENDED PILEUP IS NULL");
+            System.out.println("  Event(s): " + e.getValue().getContext(StratifiedAlignmentContext.StratifiedContextType.COMPLETE).getExtendedEventPileup().getEventStringsWithCounts(ref));
+        }
+        if ( testSkipCount==0 ) System.exit(1);
+        testSkipCount--;
         // TODO -- implement me
 
         //VariantContext vc = new MutableVariantContext("UG_indel_call", loc, alleles, genotypes, phredScaledConfidence/10.0, null, attributes);
