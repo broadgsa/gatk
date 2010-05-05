ad11201235e851d56595b609d2f3c24ba7bd3aa4
aaron <aaron@348d0f76-0448-11de-a6fe-93d51630548a>
2010-05-05 16:01:11 +0000

    adding more ROD pile-up tests
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@3301 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/playground/gatk/walkers/validation/RodSystemValidationIntegrationTest.java
+++ java/test/org/broadinstitute/sting/playground/gatk/walkers/validation/RodSystemValidationIntegrationTest.java
@@ -10,16 +10,46 @@ import java.util.Arrays;
  */
 public class RodSystemValidationIntegrationTest extends WalkerTest {
 
-    public static String baseTestString() {
-        return "-T RodSystemValidation -o %s -R " + seqLocation + "references/Homo_sapiens_assembly18/v0/Homo_sapiens_assembly18.fasta";
-    }
+    public static String baseTestString1KG() {
+            return "-T RodSystemValidation -o %s -R " + oneKGLocation + "reference/human_b36_both.fasta";
+        }
 
 
     @Test
     public void testSimpleGeliPileup() {
         WalkerTestSpec spec = new WalkerTestSpec(
-                baseTestString() + " -B eval,GeliText," + validationDataLocation + "ROD_validation/chr1.geli", 1,
-                Arrays.asList("0d338375f724dd2b5481a8606892c772"));
-        executeTest("testVCFSelect1", spec);
+                baseTestString1KG() + " -B eval,GeliText," + validationDataLocation + "ROD_validation/chr1.geli", 1,
+                Arrays.asList("832efb29a6d4e8dbae374d3eeee17d9d"));
+        executeTest("testSimpleGeliPileup", spec);
+    }
+
+    @Test
+    public void testSimpleVCFPileup() {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                baseTestString1KG() + " -B eval,VCF," + validationDataLocation + "MultiSample.vcf", 1,
+                Arrays.asList("ff4da30a3b6428bc64d61214c715efa5"));
+        executeTest("testSimpleVCFPileup", spec);
+    }
+
+    @Test
+    public void testComplexVCFPileup() {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                baseTestString1KG() + " -B eval,VCF," + validationDataLocation + "MultiSample.vcf" +
+                " -B eval,VCF," + validationDataLocation + "NA12878.chr1_10mb_11mb.slx.indels.vcf"
+                , 1,
+                Arrays.asList("52f758a70ae603213d4399220151357d"));
+        executeTest("testComplexVCFPileup", spec);
+    }
+
+    @Test
+    public void testLargeComplexVCFPileup() {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                baseTestString1KG() + " -B eval,VCF," + validationDataLocation + "MultiSample.vcf" +
+                " -B eval,VCF," + validationDataLocation + "NA12878.chr1_10mb_11mb.slx.indels.vcf" +
+                " -B eval,VCF," + validationDataLocation + "CEU_hapmap_nogt_23.vcf" +
+                " -L 1 -L 2 -L 20"
+                , 1,
+                Arrays.asList("421a27be91496e88f2f4e197dcfad0ff"));
+        executeTest("testLargeComplexVCFPileup", spec);
     }
 }
