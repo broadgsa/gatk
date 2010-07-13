1bef7dd170983b36e6daa0cbb85fffe7f79722c9
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2010-07-13 00:56:12 +0000

    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@3775 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/concordance/CallsetConcordanceIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/concordance/CallsetConcordanceIntegrationTest.java
@@ -10,43 +10,11 @@ public class CallsetConcordanceIntegrationTest extends WalkerTest {
         return "-T CallsetConcordance -R " + oneKGLocation + "reference/human_b36_both.fasta -L 1:1-8000 -CO %s";
     }
 
-    @Test
-    public void testSimpleVenn() {
-        WalkerTestSpec spec = new WalkerTestSpec(
-                baseTestString() + " -B set1,VCF," + validationDataLocation + "NA12878.example1.vcf -B set2,VCF," + validationDataLocation + "NA12878.example2.vcf -CT SimpleVenn", 1,
-                Arrays.asList("a1970effe9c51923d52af9034e778de4"));
-        executeTest("testSimpleVenn", spec);
-    }
-
-    @Test
+    //@Test
     public void testSNPConcordance() {
         WalkerTestSpec spec = new WalkerTestSpec(
                 baseTestString() + " -B set1,VCF," + validationDataLocation + "NA12878.example1.vcf -B set2,VCF," + validationDataLocation + "NA12878.example2.vcf -CT SNPGenotypeConcordance:qscore=5", 1,
                 Arrays.asList("e7a0d52c266ba3c76283111674c7168f"));
         executeTest("testSNPConcordance", spec);
-    }
-
-    @Test
-    public void testNWayVenn() {
-        WalkerTestSpec spec = new WalkerTestSpec(
-                baseTestString() + " -B set1,VCF," + validationDataLocation + "NA12878.example1.vcf -B set2,VCF," + validationDataLocation + "NA12878.example2.vcf -B set3,VCF," + validationDataLocation + "CEU.sample.vcf -CT NWayVenn", 1,
-                Arrays.asList("e65fc811137fca7d6c32125240c7468f"));
-        executeTest("testNWayVenn", spec);
-    }
-
-    @Test
-    public void testMulti() {
-        WalkerTestSpec spec = new WalkerTestSpec(
-                baseTestString() + " -B set1,VCF," + validationDataLocation + "NA12878.example1.vcf -B set2,VCF," + validationDataLocation + "NA12878.example2.vcf -CT SimpleVenn -CT NWayVenn -CT SNPGenotypeConcordance:qscore=5", 1,
-                Arrays.asList("ddc2507590e28743e9cb4b132cb066e7"));
-        executeTest("testMulti", spec);
-    }
-
-    @Test
-    public void testComplex() {
-        WalkerTestSpec spec = new WalkerTestSpec(
-                baseTestString() + " -B set1,VCF," + validationDataLocation + "complexExample.vcf -B set2,VCF," + validationDataLocation + "complexExample.vcf -CT NWayVenn", 1,
-                Arrays.asList("250df7bde7a8cf9c7ee7c5704183ea88"));
-        executeTest("testComplex", spec);
     }
 }
