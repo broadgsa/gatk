a4f8d70d8d7ce6badd3ce351be8abb55953c1a37
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2010-07-14 11:38:33 +0000

    oops, forgot to update this integration test
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@3788 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/variantutils/LiftoverVariantsIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/variantutils/LiftoverVariantsIntegrationTest.java
@@ -40,7 +40,7 @@ public class LiftoverVariantsIntegrationTest extends WalkerTest {
          WalkerTestSpec spec = new WalkerTestSpec(
                  "-T LiftoverVariants -o %s -R " + oneKGLocation + "reference/human_b36_both.fasta -B variant,vcf," + validationDataLocation + "yri.trio.gatk_glftrio.intersection.annotated.filtered.chr1.500.vcf -chain " + validationDataLocation + "b36ToHg19.broad.over.chain -dict " + oneKGLocation + "reference/human_g1k_v37.dict",
                  1,
-                 Arrays.asList("aa347e1d379ed090cbd54eef829611f9"));
+                 Arrays.asList("252cf3cebf8844fb4327cdb86bfffd23"));
          executeTest("test b36 to hg19", spec);
     }
 }
