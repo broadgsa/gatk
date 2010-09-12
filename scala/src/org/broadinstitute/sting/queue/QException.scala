81c82ce1341d55d904fbad64c3f74b72528dbc55
depristo <depristo@348d0f76-0448-11de-a6fe-93d51630548a>
2010-09-12 15:18:08 +0000

    Fix for Queue
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@4268 348d0f76-0448-11de-a6fe-93d51630548a

--- scala/src/org/broadinstitute/sting/queue/QException.scala
+++ scala/src/org/broadinstitute/sting/queue/QException.scala
@@ -1,6 +1,6 @@
 package org.broadinstitute.sting.queue
 
-import org.broadinstitute.sting.utils.StingException
+import org.broadinstitute.sting.utils.exceptions.StingException
 
 class QException(private val message: String, private val throwable: Throwable = null)
   extends StingException(message, throwable)
