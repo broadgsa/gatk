1901e3208e97b618df279c18e8ec56805d43f654
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2010-09-15 01:41:02 +0000

    Oops, ran integration tests before Guillermo committed his change to the Beagle code
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@4281 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/beagle/BeagleIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/beagle/BeagleIntegrationTest.java
@@ -41,7 +41,7 @@ public class BeagleIntegrationTest extends WalkerTest {
                         "-B:beagleR2,BEAGLE " + beagleValidationDataLocation + "inttestbgl.r2 " +
                         "-B:beagleProbs,BEAGLE " + beagleValidationDataLocation + "inttestbgl.gprobs " +
                         "-B:beaglePhased,BEAGLE " + beagleValidationDataLocation + "inttestbgl.phased " +
-                        "-o %s", 1, Arrays.asList("0508255900d28dfe017035b6f29c0385"));
+                        "-o %s", 1, Arrays.asList("caf2a19009e50931157bb4a7b1763282"));
         executeTest("test BeagleOutputToVCF", spec);
     }
    
