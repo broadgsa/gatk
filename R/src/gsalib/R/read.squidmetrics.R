50dbbdb8ab269a026e9a0bdd9bf951d01fc9d47e
kiran <kiran@348d0f76-0448-11de-a6fe-93d51630548a>
2010-11-16 22:46:07 +0000

    Retrieves per-sample or per-lane metrics from the SQUID database and populates a dataframe with the results.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@4693 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ R/src/gsalib/R/read.squidmetrics.R
@@ -0,0 +1,22 @@
+read.squidmetrics = function(project, bylane = FALSE) {
+    suppressMessages(library(ROracle));
+
+    drv = dbDriver("Oracle");
+    con = dbConnect(drv, "REPORTING/REPORTING@ora01:1521/SEQPROD");
+
+    if (bylane) {
+        rs  = dbSendQuery(con, statement = paste("SELECT * FROM ILLUMINA_PICARD_METRICS"));
+        d = fetch(rs, n=-1);
+        dbHasCompleted(rs);
+        dbClearResult(rs);
+    } else {
+        rs = dbSendQuery(con, statement = paste("SELECT * FROM ILLUMINA_SAMPLE_STATUS_AGG"));
+        d = fetch(rs, n=-1);
+        dbHasCompleted(rs);
+        dbClearResult(rs);
+    }
+
+    oraCloseDriver(drv);
+
+    d;
+}
