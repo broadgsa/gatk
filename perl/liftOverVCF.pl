8de33924e8bca107033f3f631192f626a26582ee
fromer <fromer@348d0f76-0448-11de-a6fe-93d51630548a>
2011-01-04 19:52:50 +0000

    Added recordOriginalLocation option perl script
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@4931 348d0f76-0448-11de-a6fe-93d51630548a

--- perl/liftOverVCF.pl
+++ perl/liftOverVCF.pl
@@ -12,16 +12,18 @@ my $newRef = undef;
 my $oldRef = undef;
 my $out = undef;
 my $tmp = "/tmp";
+my $recordOriginalLocation = undef;
 GetOptions( "vcf=s" => \$in,
 	    "gatk=s" => \$gatk,
 	    "chain=s" => \$chain,
 	    "newRef=s" => \$newRef,
 	    "oldRef=s" => \$oldRef,
             "out=s" => \$out,
-	    "tmp=s" => \$tmp);
+	    "tmp=s" => \$tmp,
+	    "recordOriginalLocation" => \$recordOriginalLocation);
 
-if ( !$in || !$gatk || !$chain || !$newRef || !$oldRef || !$out ) {
-    print "Usage: liftOverVCF.pl\n\t-vcf \t\t<input vcf>\n\t-gatk \t\t<path to gatk trunk>\n\t-chain \t\t<chain file>\n\t-newRef \t<path to new reference prefix; we will need newRef.dict, .fasta, and .fasta.fai>\n\t-oldRef \t<path to old reference prefix; we will need oldRef.fasta>\n\t-out \t\t<output vcf>\n\t-tmp \t\t<temp file location; defaults to /tmp>\n";
+if ( !$in || !$gatk || !$chain || !$newRef || !$oldRef || !$out || !defined($recordOriginalLocation)) {
+    print "Usage: liftOverVCF.pl\n\t-vcf \t\t<input vcf>\n\t-gatk \t\t<path to gatk trunk>\n\t-chain \t\t<chain file>\n\t-newRef \t<path to new reference prefix; we will need newRef.dict, .fasta, and .fasta.fai>\n\t-oldRef \t<path to old reference prefix; we will need oldRef.fasta>\n\t-out \t\t<output vcf>\n\t-tmp \t\t<temp file location; defaults to /tmp>\n\t-recordOriginalLocation \t\t<Should we record what the original location was in the INFO field?; defaults to false>\n";
     print "Example: ./liftOverVCF.pl\n\t-vcf /humgen/gsa-hpprojects/GATK/data/Comparisons/Validated/1kg_snp_validation/all_validation_batches.b36.vcf\n\t-chain b36ToHg19.broad.over.chain\n\t-out lifted.hg19.vcf\n\t-gatk /humgen/gsa-scr1/ebanks/Sting_dev\n\t-newRef /seq/references/Homo_sapiens_assembly19/v0/Homo_sapiens_assembly19\n\t-oldRef /humgen/1kg/reference/human_b36_both\n";
     exit(1);
 }
@@ -34,7 +36,7 @@ my $unsorted_vcf = "$tmp_prefix.unsorted.vcf";
 
 # lift over the file
 print "Lifting over the vcf...";
-my $cmd = "java -jar $gatk/dist/GenomeAnalysisTK.jar -T LiftoverVariants -R $oldRef.fasta -B:variant,vcf $in -o $unsorted_vcf -chain $chain -dict $newRef.dict";
+my $cmd = "java -jar $gatk/dist/GenomeAnalysisTK.jar -T LiftoverVariants -R $oldRef.fasta -B:variant,vcf $in -o $unsorted_vcf -chain $chain -dict $newRef.dict -recordOriginalLocation $recordOriginalLocation";
 system($cmd);
 
 # we need to sort the lifted over file now
