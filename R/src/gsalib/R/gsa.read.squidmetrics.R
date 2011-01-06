ab143c82af6186abc45ead4e8ac3ef002537e222
kiran <kiran@348d0f76-0448-11de-a6fe-93d51630548a>
2011-01-06 15:32:32 +0000

    Selects only the project requested via the Oracle command, rather than selecting everything and then subsetting.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@4949 348d0f76-0448-11de-a6fe-93d51630548a

--- R/src/gsalib/R/gsa.read.squidmetrics.R
+++ R/src/gsalib/R/gsa.read.squidmetrics.R
@@ -5,12 +5,18 @@ gsa.read.squidmetrics = function(project, bylane = FALSE) {
     con = dbConnect(drv, "REPORTING/REPORTING@ora01:1521/SEQPROD");
 
     if (bylane) {
-        rs  = dbSendQuery(con, statement = paste("SELECT * FROM ILLUMINA_PICARD_METRICS"));
+        statement = paste("SELECT * FROM ILLUMINA_PICARD_METRICS WHERE \"Project\" = '", project, "'", sep="");
+        print(statement);
+
+        rs  = dbSendQuery(con, statement = statement);
         d = fetch(rs, n=-1);
         dbHasCompleted(rs);
         dbClearResult(rs);
     } else {
-        rs = dbSendQuery(con, statement = paste("SELECT * FROM ILLUMINA_SAMPLE_STATUS_AGG"));
+        statement = paste("SELECT * FROM ILLUMINA_SAMPLE_STATUS_AGG WHERE \"Project\" = '", project, "'", sep="");
+        print(statement);
+
+        rs = dbSendQuery(con, statement = statement);
         d = fetch(rs, n=-1);
         dbHasCompleted(rs);
         dbClearResult(rs);
