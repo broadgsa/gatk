a22ea53665c3481d671715727dfd2d394054269d
corin <corin@348d0f76-0448-11de-a6fe-93d51630548a>
2011-02-03 18:33:29 +0000

    Updated template for the MPG pipeline's queue script runner.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@5187 348d0f76-0448-11de-a6fe-93d51630548a

--- shell/queue.sh
+++ shell/queue.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 SAMPLE_SET=$1
-STING_HOME=/humgen/gsa-firehose2/pipeline/repositories/StingProduction
+STING_HOME=/humgen/gsa-pipeline/.repository
 REFSEQ_TABLE=/humgen/gsa-hpprojects/GATK/data/Annotations/refseq/refGene-big-table-hg19.txt
-JOB_QUEUE=gsa
-SHORT_QUEUE=gsa
+JOB_QUEUE=week
+SHORT_QUEUE=hour
 TMP_DIR=$PWD/tmp
 
 mkdir $PWD/tmp
@@ -14,4 +14,4 @@ use R-2.10
 use Oracle-full-client
 use .cx-oracle-5.0.2-python-2.6.5-oracle-full-client-11.1
 
-java -Djava.io.tmpdir="$TMP_DIR" -jar "$STING_HOME"/dist/Queue.jar -jobQueue "$JOB_QUEUE" -shortJobQueue "$SHORT_QUEUE" -jobProject "$SAMPLE_SET" -jobPrefix "$SAMPLE_SET" -tearScript ~/Sting/R/DataProcessingReport/GetTearsheetStats.R -S ~/Sting/scala/qscript/fullCallingPipeline.q -Y "$SAMPLE_SET".yaml -refseqTable "$REFSEQ_TABLE" --gatkjar "$STING_HOME"/dist/GenomeAnalysisTK.jar -log queue_log.txt -statusTo corin -bsub $2
+java -Djava.io.tmpdir="$TMP_DIR" -jar "$STING_HOME"/dist/Queue.jar -jobQueue "$JOB_QUEUE" -shortJobQueue "$SHORT_QUEUE" -jobProject "$SAMPLE_SET" -jobPrefix "$SAMPLE_SET" -tearScript "$STING_HOME"/R/DataProcessingReport/GetTearsheetStats.R -S "$STING_HOME"/scala/qscript/playground/fullCallingPipeline.q -Y "$SAMPLE_SET".yaml -refseqTable "$REFSEQ_TABLE" --gatkjar "$STING_HOME"/dist/GenomeAnalysisTK.jar -log queue_log.txt -statusTo corin -bsub $2
