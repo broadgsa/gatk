11ea321b39ae1c25a3f8b970a857c6914aa5d55e
depristo <depristo@348d0f76-0448-11de-a6fe-93d51630548a>
2011-02-04 22:23:15 +0000

    Trivial header cleanup
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@5198 348d0f76-0448-11de-a6fe-93d51630548a

--- java/src/org/broadinstitute/sting/utils/Utils.java
+++ java/src/org/broadinstitute/sting/utils/Utils.java
@@ -25,13 +25,7 @@
 
 package org.broadinstitute.sting.utils;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import net.sf.samtools.util.StringUtil;
 
