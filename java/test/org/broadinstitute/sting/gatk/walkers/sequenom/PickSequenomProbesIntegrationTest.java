45081c32d7d593523993f99f692d56fa7f571dc4
ebanks <ebanks@348d0f76-0448-11de-a6fe-93d51630548a>
2011-04-28 13:30:57 +0000

    continuing from last night, the integration tests weren't covering the right behavior either
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@5706 348d0f76-0448-11de-a6fe-93d51630548a

--- java/test/org/broadinstitute/sting/gatk/walkers/sequenom/PickSequenomProbesIntegrationTest.java
+++ java/test/org/broadinstitute/sting/gatk/walkers/sequenom/PickSequenomProbesIntegrationTest.java
@@ -17,25 +17,18 @@ public class PickSequenomProbesIntegrationTest extends WalkerTest {
 
     @Test
     public void testProbesUsingDbSNPMask() {
-        String testVCF = validationDataLocation + "pickSeqIntegrationTest.vcf";
-        String testArgs = "-snp_mask " + GATKDataLocation + "/dbsnp_130_b36.rod -R "
-                + b36KGReference + " -omitWindow -nameConvention "
-                + "-project_id 1kgp3_s4_lf -T PickSequenomProbes -L " + validationDataLocation +
-                "pickSeqIntegrationTest.interval_list -B:input,VCF "+testVCF+" -o %s";
-        WalkerTestSpec spec = new WalkerTestSpec(testArgs, 1,
-                Arrays.asList("e50162490d0597c8f00d2d20b1463661"));
-        executeTest("Test probes", spec);
-    }
 
-    @Test
-    public void testProbesUsingDbSNPMaskWithNMW1() {
-    String testVCF = validationDataLocation + "pickSeqIntegrationTest.vcf";
-        String testArgs = "-snp_mask " + GATKDataLocation + "/dbsnp_130_b36.rod -R "
+        String md5 = "46d53491af1d3aa0ee1f1e13d68b732d";
+        String testVCF = validationDataLocation + "pickSeqIntegrationTest.vcf";
+
+        String testArgs = "-snp_mask " + validationDataLocation + "pickSeqIntegrationTest.bed -R "
                 + b36KGReference + " -omitWindow -nameConvention "
-                + "-nmw 1 -project_id 1kgp3_s4_lf -T PickSequenomProbes -L " + validationDataLocation +
-	    "pickSeqIntegrationTest.interval_list -B:input,VCF "+testVCF+" -o %s";
-        WalkerTestSpec spec = new WalkerTestSpec(testArgs, 1,
-						 Arrays.asList("e50162490d0597c8f00d2d20b1463661"));
-        executeTest("Test probes", spec);
+                + "-project_id 1kgp3_s4_lf -T PickSequenomProbes -B:input,VCF "+testVCF+" -o %s";
+        WalkerTestSpec spec1 = new WalkerTestSpec(testArgs, 1, Arrays.asList(md5));
+        executeTest("Test probes", spec1);
+
+        testArgs += " -nmw 1";
+        WalkerTestSpec spec2 = new WalkerTestSpec(testArgs, 1, Arrays.asList(md5));
+        executeTest("Test probes", spec2);
     }
 }
