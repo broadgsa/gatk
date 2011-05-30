0aef5c0074318610f0bbbb64cd93751ad02440b6
delangel <delangel@348d0f76-0448-11de-a6fe-93d51630548a>
2011-05-30 14:05:39 +0000

    Totaly experimental, possibly useless annotation that logs # of MQ0 reads / total depth, TBD if VQSR can use it.
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@5905 348d0f76-0448-11de-a6fe-93d51630548a

---
+++ java/src/org/broadinstitute/sting/gatk/walkers/annotator/MappingQualityZeroFraction.java
@@ -0,0 +1,60 @@
+package org.broadinstitute.sting.gatk.walkers.annotator;
+
+import org.broad.tribble.util.variantcontext.VariantContext;
+import org.broad.tribble.vcf.VCFConstants;
+import org.broad.tribble.vcf.VCFHeaderLineType;
+import org.broad.tribble.vcf.VCFInfoHeaderLine;
+import org.broadinstitute.sting.gatk.contexts.AlignmentContext;
+import org.broadinstitute.sting.gatk.contexts.ReferenceContext;
+import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
+import org.broadinstitute.sting.gatk.walkers.annotator.interfaces.ExperimentalAnnotation;
+import org.broadinstitute.sting.gatk.walkers.annotator.interfaces.InfoFieldAnnotation;
+import org.broadinstitute.sting.utils.pileup.PileupElement;
+import org.broadinstitute.sting.utils.pileup.ReadBackedPileup;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+public class MappingQualityZeroFraction implements InfoFieldAnnotation, ExperimentalAnnotation {
+
+    public Map<String, Object> annotate(RefMetaDataTracker tracker, ReferenceContext ref, Map<String, AlignmentContext> stratifiedContexts, VariantContext vc) {
+        if ( stratifiedContexts.size() == 0 )
+            return null;
+
+        int mq0 = 0;
+        int depth = 0;
+        for ( Map.Entry<String, AlignmentContext> sample : stratifiedContexts.entrySet() ) {
+            AlignmentContext context = sample.getValue();
+            depth += context.size();
+             ReadBackedPileup pileup = null;
+            if (context.hasExtendedEventPileup())
+                pileup = context.getExtendedEventPileup();
+            else if (context.hasBasePileup())
+                pileup = context.getBasePileup();
+
+            if (pileup != null) {
+                for (PileupElement p : pileup ) {
+                    if ( p.getMappingQual() == 0 )
+                        mq0++;
+                }
+            }
+        }
+        if (depth > 0) {
+            double mq0f = (double)mq0 / (double )depth;
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put(getKeyNames().get(0), String.format("%1.4f", mq0f));
+            return map;
+        }
+        else
+            return null;
+    }
+
+    public List<String> getKeyNames() { return Arrays.asList("MQ0Fraction"); }
+
+    public List<VCFInfoHeaderLine> getDescriptions() { return Arrays.asList(new VCFInfoHeaderLine(getKeyNames().get(0), 1, VCFHeaderLineType.Integer, "Fraction of Mapping Quality Zero Reads")); }
+}
