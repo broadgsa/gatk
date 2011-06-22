0d07c979e936c8f5a276e812cdbcae9278d2d635
droazen <droazen@348d0f76-0448-11de-a6fe-93d51630548a>
2011-06-22 22:55:50 +0000

    added comments on how to use this very useful script!
    git-svn-id: file:///humgen/gsa-scr1/gsa-engineering/svn_contents/trunk@6066 348d0f76-0448-11de-a6fe-93d51630548a

--- lua/updateSampleList.lua
+++ lua/updateSampleList.lua
@@ -1,3 +1,9 @@
+--[[
+-- Updates a list of BAM files to the latest version in the picard aggregation path
+-- Usage:
+--
+-- lua updateSampleList.lua samples.list > updated_samples.list
+ ]]
 function latestVersion(sample)
   local version = tonumber(sample:match("/v(%d+)/"))
   f = io.open(sample)
