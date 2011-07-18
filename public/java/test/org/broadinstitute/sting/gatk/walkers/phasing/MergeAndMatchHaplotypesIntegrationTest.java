8167aba601c5d660e17b2407f090e3d9c61884e0
Kiran V Garimella <kiran@broadinstitute.org>
2011-07-17 22:47:32 -0400

    Moved (poorly named) MergeAndMatchHaplotypes to public.  Added integration test

---
+++ public/java/test/org/broadinstitute/sting/gatk/walkers/phasing/MergeAndMatchHaplotypesIntegrationTest.java
@@ -0,0 +1,28 @@
+package org.broadinstitute.sting.gatk.walkers.phasing;
+
+import org.broadinstitute.sting.WalkerTest;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class MergeAndMatchHaplotypesIntegrationTest extends WalkerTest {
+    private static String mergeAndMatchHaplotypesTestDataRoot = validationDataLocation + "/MergeByHaplotypes";
+    private static String fundamentalTestPBTVCF = mergeAndMatchHaplotypesTestDataRoot + "/" + "FundamentalsTest.pbt.vcf";
+    private static String fundamentalTestRBPVCF = mergeAndMatchHaplotypesTestDataRoot + "/" + "FundamentalsTest.pbt.rbp.vcf";
+
+    @Test
+    public void testBasicFunctionality() {
+        WalkerTestSpec spec = new WalkerTestSpec(
+                buildCommandLine(
+                        "-T MergeAndMatchHaplotypes",
+                        "-R " + b37KGReference,
+                        "-B:pbt,VCF " + fundamentalTestPBTVCF,
+                        "-B:rbp,VCF " + fundamentalTestRBPVCF,
+                        "-o %s"
+                ),
+                1,
+                Arrays.asList("")
+        );
+        executeTest("testBasicMergeAndMatchHaplotypesFunctionality", spec);
+    }
+}
