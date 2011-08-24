16caca0822dfb08271f2ed30a31a8ff4c73da60f
Mauricio Carneiro <carneiro@broadinstitute.org>
2011-08-24 17:02:44 -0400

    BLASR BAMs and new BWA parameters
    *Added the functions to turn a BLASR generated BAM file into a usable BAM file.
    *Modified the bwa parameters according to test results from NA12878 pb2k dataset.

--- public/scala/src/org/broadinstitute/sting/queue/extensions/picard/ReorderSam.scala
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/picard/ReorderSam.scala
@@ -11,7 +11,7 @@ import java.io.File
  */
 class ReorderSam extends org.broadinstitute.sting.queue.function.JavaCommandLineFunction with PicardBamFunction {
   analysisName = "ReorderSam"
-  javaMainClass = "net.sf.picard.sam.SortSam"
+  javaMainClass = "net.sf.picard.sam.ReorderSam"
 
   @Input(doc="Input file (bam or sam) to extract reads from.", shortName = "input", fullName = "input_bam_files", required = true)
   var input: List[File] = Nil
@@ -22,27 +22,27 @@ class ReorderSam extends org.broadinstitute.sting.queue.function.JavaCommandLine
   @Output(doc="The output bam index", shortName = "out_index", fullName = "output_bam_index_file", required = false)
   var outputIndex: File = _
 
-  @Argument(doc="Reference sequence to reorder reads to match.", shortName = "input", fullName = "input_bam_files", required = true)
-  var reference: File = _
+  @Argument(doc="Reference sequence to reorder reads to match.", shortName = "ref", fullName = "sort_reference", required = true)
+  var sortReference: File = _
 
   @Argument(doc="If true, then allows only a partial overlap of the BAM contigs with the new reference sequence contigs. By default, this tool requires a corresponding contig in the new reference for each read contig.", shortName = "aic", fullName = "allow_incomplete_concordance", required = false)
-  var ALLOW_INCOMPLETE_DICT_CONCORDANCE: Boolean = false
+  var ALLOW_INCOMPLETE_DICT_CONCORDANCE: Boolean = _
 
   @Argument(doc="If true, then permits mapping from a read contig to a new reference contig with the same name but a different length. Highly dangerous, only use if you know what you are doing.", shortName = "acld", fullName = "allow_contig_length_discordance", required = false)
-  var ALLOW_CONTIG_LENGTH_DISCORDANCE: Boolean = false
+  var ALLOW_CONTIG_LENGTH_DISCORDANCE: Boolean = _
 
   override def freezeFieldValues() {
     super.freezeFieldValues()
-    if (outputIndex == null && output != null && output.toString.endsWith(".bam"))
+    if (outputIndex == null && output != null)
       outputIndex = new File(output.getName.stripSuffix(".bam") + ".bai")
   }
 
   override def inputBams = input
   override def outputBam = output
-  this.createIndex = Some(outputIndex != null)
+  this.createIndex = Some(true)
   this.sortOrder = null
   override def commandLine = super.commandLine +
-    " REFERENCE=" + reference +
-    conditionalParameter (ALLOW_INCOMPLETE_DICT_CONCORDANCE, " ALLOW_INCOMPLETE_DICT_CONCORDANCE=true")
-    conditionalParameter (ALLOW_CONTIG_LENGTH_DISCORDANCE, " ALLOW_CONTIG_LENGTH_DISCORDANCE=true")
+    " REFERENCE=" + sortReference +
+    optional(" ALLOW_INCOMPLETE_DICT_CONCORDANCE=", ALLOW_INCOMPLETE_DICT_CONCORDANCE)
+    optional(" ALLOW_CONTIG_LENGTH_DISCORDANCE=", ALLOW_CONTIG_LENGTH_DISCORDANCE)
 }
