d1e70d6ec21a8015596ac7aa4c3163c6b4af28ef
Mark DePristo <depristo@broadinstitute.org>
2011-10-06 18:29:26 -0700

    Removed Nx counting of reads in metrics with -nt > 1

--- public/java/src/org/broadinstitute/sting/gatk/executive/ShardTraverser.java
+++ public/java/src/org/broadinstitute/sting/gatk/executive/ShardTraverser.java
@@ -66,15 +66,13 @@ public class ShardTraverser implements Callable {
                     microScheduler.getReadIterator(shard),
                     shard.getGenomeLocs(),
                     microScheduler.engine.getSampleDB().getSampleNames()); // todo: microScheduler.engine is protected - is it okay to user it here?
-            ShardDataProvider dataProvider = null;
 
             for(WindowMaker.WindowMakerIterator iterator: windowMaker) {
-                dataProvider = new LocusShardDataProvider(shard,iterator.getSourceInfo(),microScheduler.getEngine().getGenomeLocParser(),iterator.getLocus(),iterator,microScheduler.reference,microScheduler.rods);
+                final ShardDataProvider dataProvider = new LocusShardDataProvider(shard,iterator.getSourceInfo(),microScheduler.getEngine().getGenomeLocParser(),iterator.getLocus(),iterator,microScheduler.reference,microScheduler.rods);
                 accumulator = traversalEngine.traverse( walker, dataProvider, accumulator );
                 dataProvider.close();
             }
 
-            if (dataProvider != null) dataProvider.close();
             windowMaker.close();
             outputMergeTask = outputTracker.closeStorage();
 
