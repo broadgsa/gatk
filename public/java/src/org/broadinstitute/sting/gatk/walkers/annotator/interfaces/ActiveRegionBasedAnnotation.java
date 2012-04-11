7aa654d13f1cf734bb9faa7c9d5053327b61cdb0
Eric Banks <ebanks@broadinstitute.org>
2012-04-11 13:49:09 -0400

    New interface for some dev work that Ryan and I are doing; only accessible from private walkers right now

---
+++ public/java/src/org/broadinstitute/sting/gatk/walkers/annotator/interfaces/ActiveRegionBasedAnnotation.java
@@ -0,0 +1,18 @@
+package org.broadinstitute.sting.gatk.walkers.annotator.interfaces;
+
+import org.broadinstitute.sting.utils.codecs.vcf.VCFInfoHeaderLine;
+import org.broadinstitute.sting.utils.sam.GATKSAMRecord;
+import org.broadinstitute.sting.utils.variantcontext.Allele;
+import org.broadinstitute.sting.utils.variantcontext.VariantContext;
+
+import java.util.List;
+import java.util.Map;
+
+// TODO -- make this an abstract class when we move away from InfoFieldAnnotation
+public interface ActiveRegionBasedAnnotation {
+    // return annotations for the given contexts split by sample and then allele
+    public abstract Map<String, Object> annotate(final Map<String, Map<Allele, List<GATKSAMRecord>>> stratifiedContexts, final VariantContext vc);
+
+    // return the descriptions used for the VCF INFO meta field
+    public abstract List<VCFInfoHeaderLine> getDescriptions();
+}
