77bdcd143efd2c37a6d09e0a9d896d9cfb64b6cb
Mark DePristo <depristo@broadinstitute.org>
2012-09-04 07:26:01 -0400

    Done GSA-282: Unindexed traversals crash if a read goes off the end of a contig
    -- Already fixed in the codebase.  Added unindexed bam and integration tests to ensure this is fine going forward.

--- public/java/test/org/broadinstitute/sting/gatk/walkers/PileupWalkerIntegrationTest.java
+++ public/java/test/org/broadinstitute/sting/gatk/walkers/PileupWalkerIntegrationTest.java
@@ -16,13 +16,27 @@ public class PileupWalkerIntegrationTest extends WalkerTest {
         executeTest("Testing the standard (no-indel) pileup on three merged FHS pools with 27 deletions in 969 bases", spec);
     }
 
+
+
+    private final static String SingleReadAligningOffChromosome1MD5 = "4a45fe1f85aaa8c4158782f2b6dee2bd";
     @Test
     public void testSingleReadAligningOffChromosome1() {
         String gatk_args = "-T Pileup "
                 + " -I " + privateTestDir + "readOffb37contig1.bam"
                 + " -R " + b37KGReference
                 + " -o %s";
-        WalkerTestSpec spec = new WalkerTestSpec(gatk_args, 1, Arrays.asList("4a45fe1f85aaa8c4158782f2b6dee2bd"));
+        WalkerTestSpec spec = new WalkerTestSpec(gatk_args, 1, Arrays.asList(SingleReadAligningOffChromosome1MD5));
         executeTest("Testing single read spanning off chromosome 1", spec);
+    }
+
+    @Test
+    public void testSingleReadAligningOffChromosome1NoIndex() {
+        String gatk_args = "-T Pileup "
+                + " -I " + privateTestDir + "readOffb37contig1.noIndex.bam"
+                + " -R " + b37KGReference
+                + " -U ALLOW_UNINDEXED_BAM"
+                + " -o %s";
+        WalkerTestSpec spec = new WalkerTestSpec(gatk_args, 1, Arrays.asList(SingleReadAligningOffChromosome1MD5));
+        executeTest("Testing single read spanning off chromosome 1 unindexed", spec);
     }
 }
