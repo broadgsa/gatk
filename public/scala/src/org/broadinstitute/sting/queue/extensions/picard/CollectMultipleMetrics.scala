12f2d8689e5c596015387d646aca1231db077a71
Guillermo del Angel <delangel@broadinstitute.org>
2012-10-11 11:09:49 -0400

    Resolve [DEV-7]: add single-sample VCF calling at end of FASTQ-BAM pipeline. Initial steps of [DEV-4]: queue extensions for Picard QC metrics

---
+++ public/scala/src/org/broadinstitute/sting/queue/extensions/picard/CollectMultipleMetrics.scala
@@ -0,0 +1,36 @@
+package org.broadinstitute.sting.queue.extensions.picard
+
+import org.broadinstitute.sting.commandline.{Argument, Output, Input}
+import java.io.File
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: delangel
+ * Date: 10/10/12
+ * Time: 10:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class CollectMultipleMetrics extends org.broadinstitute.sting.queue.function.JavaCommandLineFunction with PicardBamFunction{
+  analysisName = "CalculateMultipleMetrics"
+  javaMainClass = "net.sf.picard.sam.CalculateMultipleMetrics"
+
+  @Input(doc="The input SAM or BAM files to analyze.  Must be coordinate sorted.", shortName = "input", fullName = "input_bam_files", required = true)
+  var input: Seq[File] = Nil
+
+  @Output(doc="The output file to write statistics to", shortName = "output", fullName = "output_file", required = true)
+  var output: File = _
+
+  @Argument(doc="Reference file", shortName = "reference", fullName = "reference", required = true)
+  var reference: File = _
+
+  override def inputBams = input
+  override def outputBam = output
+  override def commandLine = super.commandLine +
+    required("REFERENCE_SEQUENCE=" + reference) +
+    required("ASSUME_SORTED=true") +
+    required("PROGRAM=QualityScoreDistribution") +
+    required("PROGRAM=MeanQualityByCycle") +
+    required("PROGRAM=CollectAlignmentSummaryMetrics" )
+
+
+}
