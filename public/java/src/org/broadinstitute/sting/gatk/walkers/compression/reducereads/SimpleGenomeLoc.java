4efa8d9cc2b34d17dc83cdc553c8fc72c4128063
Mauricio Carneiro <carneiro@broadinstitute.org>
2012-10-24 16:57:08 -0400

    Co-Reduction implementation in ReduceReads
    ReduceReads now co-reduces bams if they're passed in toghether with multiple -I. Co-reduction forces every variant region in one sample to be a variant region in all samples.
    Also:
      * Added integrationtest for co-reduction
      * Fixed bug with new no-recalculation implementation of the marksites object where the last object wasn't being removed after finalizing a variant region (updated MD5's accordingly)
    
    DEV-200 #resolve #time 8m

--- public/java/src/org/broadinstitute/sting/gatk/walkers/compression/reducereads/SimpleGenomeLoc.java
+++ public/java/src/org/broadinstitute/sting/gatk/walkers/compression/reducereads/SimpleGenomeLoc.java
@@ -1,6 +1,10 @@
 package org.broadinstitute.sting.gatk.walkers.compression.reducereads;
 
+import com.google.java.contract.Requires;
 import org.broadinstitute.sting.utils.GenomeLoc;
+import org.broadinstitute.sting.utils.exceptions.ReviewedStingException;
+
+import java.util.SortedSet;
 
 /**
  * GenomeLocs are very useful objects to keep track of genomic locations and perform set operations
@@ -26,5 +30,44 @@ public class SimpleGenomeLoc extends GenomeLoc {
 
     public boolean isFinished() {
         return finished;
+    }
+
+    @Requires("a != null && b != null")
+    public static SimpleGenomeLoc merge(SimpleGenomeLoc a, SimpleGenomeLoc b) throws ReviewedStingException {
+        if(GenomeLoc.isUnmapped(a) || GenomeLoc.isUnmapped(b)) {
+            throw new ReviewedStingException("Tried to merge unmapped genome locs");
+        }
+
+        if (!(a.contiguousP(b))) {
+            throw new ReviewedStingException("The two genome locs need to be contiguous");
+        }
+
+
+        return new SimpleGenomeLoc(a.getContig(), a.contigIndex,
+                Math.min(a.getStart(), b.getStart()),
+                Math.max(a.getStop(), b.getStop()),
+                a.isFinished());
+    }
+
+    /**
+     * Merges a list of *sorted* *contiguous* locs into one
+     *
+     * @param sortedLocs a sorted list of contiguous locs
+     * @return one merged loc
+     */
+    public static SimpleGenomeLoc merge(SortedSet<SimpleGenomeLoc> sortedLocs) {
+        SimpleGenomeLoc previousLoc = null;
+        for (SimpleGenomeLoc loc : sortedLocs) {
+            if (loc.isUnmapped()) {
+                throw new ReviewedStingException("Tried to merge unmapped genome locs");
+            }
+            if (previousLoc != null && !previousLoc.contiguousP(loc)) {
+                throw new ReviewedStingException("The genome locs need to be contiguous");
+            }
+            previousLoc = loc;
+        }
+        SimpleGenomeLoc firstLoc = sortedLocs.first();
+        SimpleGenomeLoc lastLoc = sortedLocs.last();
+        return merge(firstLoc, lastLoc);
     }
 }
