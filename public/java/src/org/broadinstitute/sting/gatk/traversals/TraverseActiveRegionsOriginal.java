9ce5ed93e96f2fb8a26514f117501706a49e48d4
Mark DePristo <depristo@broadinstitute.org>
2013-01-10 20:48:55 -0500

    Fix final merge conflicts

--- public/java/src/org/broadinstitute/sting/gatk/traversals/TraverseActiveRegionsOriginal.java
+++ public/java/src/org/broadinstitute/sting/gatk/traversals/TraverseActiveRegionsOriginal.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2012 The Broad Institute
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package org.broadinstitute.sting.gatk.traversals;
 
 import org.apache.log4j.Logger;
@@ -232,6 +257,6 @@ public class TraverseActiveRegionsOriginal<M,T> extends TraverseActiveRegions<M,
      * Ugly for now but will be cleaned up when we push this functionality more into the engine
      */
     public T endTraversal( final Walker<M,T> walker, T sum) {
-        return processActiveRegions((ActiveRegionWalker<M,T>)walker, sum, Integer.MAX_VALUE, null);
+        return processActiveRegions((ActiveRegionWalker<M, T>) walker, sum, Integer.MAX_VALUE, null);
     }
 }
