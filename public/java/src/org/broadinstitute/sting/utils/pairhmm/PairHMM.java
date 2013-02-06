ec26df32b33e909f6c859d7d321b9f1ea51559e5
Mark DePristo <depristo@broadinstitute.org>
2013-02-06 17:27:12 -0500

    Preliminary version of LoglessCachingPairHMM that avoids positive likelihoods
    -- Would have been squashed but could not because of subsequent deletion of Caching and Exact/Original PairHMMs
    -- Actual working unit tests for PairHMMUnitTest
    -- Fixed incorrect logic in how I compared hmm results to the theoretical and exact results
    -- PairHMM has protected variables used throughout the subclasses

--- public/java/src/org/broadinstitute/sting/utils/pairhmm/PairHMM.java
+++ public/java/src/org/broadinstitute/sting/utils/pairhmm/PairHMM.java
@@ -33,7 +33,6 @@ import com.google.java.contract.Requires;
  * User: rpoplin
  * Date: 10/16/12
  */
-
 public abstract class PairHMM {
     protected static final Byte MAX_CACHED_QUAL = Byte.MAX_VALUE;
     protected static final byte DEFAULT_GOP = (byte) 45;
@@ -41,11 +40,11 @@ public abstract class PairHMM {
 
     public enum HMM_IMPLEMENTATION {
         /* Very slow implementation which uses very accurate log10 sum functions. Only meant to be used as a reference test implementation */
-        EXACT,
+        EXACT, // TODO -- merge with original, using boolean parameter to determine accuracy of HMM
         /* PairHMM as implemented for the UnifiedGenotyper. Uses log10 sum functions accurate to only 1E-4 */
         ORIGINAL,
         /* Optimized version of the PairHMM which caches per-read computations */
-        CACHING,
+        CACHING, // TODO -- delete me
         /* Optimized version of the PairHMM which caches per-read computations and operations in real space to avoid costly sums of log10'ed likelihoods */
         LOGLESS_CACHING
     }
@@ -53,12 +52,28 @@ public abstract class PairHMM {
     protected double[][] matchMetricArray = null;
     protected double[][] XMetricArray = null;
     protected double[][] YMetricArray = null;
+    protected int X_METRIC_LENGTH, Y_METRIC_LENGTH;
+    protected int nPotentialXStarts = 0;
 
-    public abstract void initialize( final int READ_MAX_LENGTH, final int HAPLOTYPE_MAX_LENGTH );
+    public void initialize( final int READ_MAX_LENGTH, final int HAPLOTYPE_MAX_LENGTH ) {
+        // M, X, and Y arrays are of size read and haplotype + 1 because of an extra column for initial conditions and + 1 to consider the final base in a non-global alignment
+        X_METRIC_LENGTH = READ_MAX_LENGTH + 2;
+        Y_METRIC_LENGTH = HAPLOTYPE_MAX_LENGTH + 2;
+
+        // the number of potential start sites for the read against the haplotype
+        // for example, a 3 bp read against a 5 bp haplotype could potentially start at 1, 2, 3 = 5 - 3 + 1 = 3
+        nPotentialXStarts = HAPLOTYPE_MAX_LENGTH - READ_MAX_LENGTH + 1;
+
+        // TODO -- add meaningful runtime checks on params
+
+        matchMetricArray = new double[X_METRIC_LENGTH][Y_METRIC_LENGTH];
+        XMetricArray = new double[X_METRIC_LENGTH][Y_METRIC_LENGTH];
+        YMetricArray = new double[X_METRIC_LENGTH][Y_METRIC_LENGTH];
+    }
 
     @Requires({"readBases.length == readQuals.length", "readBases.length == insertionGOP.length", "readBases.length == deletionGOP.length",
                "readBases.length == overallGCP.length", "matchMetricArray!=null", "XMetricArray!=null", "YMetricArray!=null"})
-    @Ensures({"!Double.isInfinite(result)", "!Double.isNaN(result)"}) // Result should be a proper log10 likelihood
+    @Ensures({"!Double.isInfinite(result)", "!Double.isNaN(result)", "result <= 0.0"}) // Result should be a proper log10 likelihood
     public abstract double computeReadLikelihoodGivenHaplotypeLog10( final byte[] haplotypeBases,
                                                                      final byte[] readBases,
                                                                      final byte[] readQuals,
