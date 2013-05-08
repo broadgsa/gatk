224d6a9fa132234be9c852c8a55a569b399670bf
sathibault <thibault@gmvhdl.com>
2013-05-08 11:01:20 -0500

    Adding Convey HC-1 HMM acceleration

---
+++ public/java/src/org/broadinstitute/sting/utils/pairhmm/BatchPairHMM.java
@@ -0,0 +1,16 @@
+package org.broadinstitute.sting.utils.pairhmm;
+
+import java.util.List;
+
+import org.broadinstitute.sting.utils.haplotype.Haplotype;
+
+public interface BatchPairHMM {
+    public void batchAdd(final List<Haplotype> haplotypes, 
+			 final byte[] readBases,
+			 final byte[] readQuals,
+			 final byte[] insertionGOP,
+			 final byte[] deletionGOP,
+			 final byte[] overallGCP);
+
+    public double[] batchResult();
+}
