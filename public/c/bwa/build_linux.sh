2aa92935968227c4e523340a0af4a982344b8c36
Khalid Shakir <kshakir@broadinstitute.org>
2014-04-02 03:55:21 +0800

    After renaming the packages, instead of updating the JNI library used for testing bwa, moving the classes to the archive. NOTE: The migrated READEME.md has been added that will allow others to possibly ressurect this code as needed.

--- public/c/bwa/build_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export BWA_HOME="${PWD}/bwasvn47"
-export JAVA_INCLUDE="${JAVA_HOME}/include"
-export JAVA_PLATFORM_INCLUDE="${JAVA_HOME}/include/linux"
-export TARGET_LIB="libbwa.so"
-export EXTRA_LIBS="-lc -lz -lstdc++ -lpthread"
-export LIBTOOL_COMMAND="g++ -shared -Wl,-soname,libbwa.so"
-make 
--- public/c/bwa/build_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export BWA_HOME="${PWD}/bwasvn47"
-export JAVA_INCLUDE="${JAVA_HOME}/include"
-export JAVA_PLATFORM_INCLUDE="${JAVA_HOME}/include/darwin"
-export TARGET_LIB="libbwa.dylib"
-export EXTRA_LIBS="-lc -lz -lstdc++"
-export LIBTOOL_COMMAND="libtool -dynamic"
-make 
